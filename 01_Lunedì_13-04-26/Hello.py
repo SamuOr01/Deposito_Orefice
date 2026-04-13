12c98ec9215ddba4ec241e1ae257effab80e8d08
Samuele Orefice <samu.or01@gmail.com>
2026-04-13 23:10:04 +0200

    Add files via upload

---
+++ 01_Lunedì_13-04-26/Hello.py
@@ -0,0 +1,10 @@
+print("Ciao")
+
+nome = "Samuele"
+età = 24
+
+print("Ciao, il mio nome è " + nome + " e ho " + str(età) + " anni")
+
+nome = input("Inserisci il tuo nome: ")
+età = int(input("Inserisci la tua età: "))
+print("Ciao, " + nome + "! Benvenuto in Python!")
