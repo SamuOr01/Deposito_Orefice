29b09e42c85a4d481c908000c04cd6c1343c4498
Samuele Orefice <samu.or01@gmail.com>
2026-04-14 21:41:13 +0200

    Delete 02_Martedì_14-04-2026 directory

--- 02_Martedì_14-04-2026/Esercizi/if/Esercizio_1.py
+++
@@ -1,9 +0,0 @@
-print("Benvenuto, l'accesso è consentito solo ai maggiorenni")
-età = int(input("Quanti anni hai?: "))
-
-if età >= 18: # Se inserisci 18 anni o più puoi entrare
-    print("Prego puoi entrare")
-elif età <= 0: # Se inserisci 0 anni o meno, l'età è invalida
-    print("Età non valida!")
-else: # Se inserisci un numero tra 1 e 17 anni, non puoi entrare perché sei minorenne
-    print("Mi dispiace non puoi entrare")
