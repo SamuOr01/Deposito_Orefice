e843245eee9093d49b3b13f73d9a66dfd9ff59be
Samuele Orefice <samu.or01@gmail.com>
2026-04-16 15:00:49 +0200

    Delete 03_Mercoledì_14-04-2026 directory

--- 03_Mercoledì_14-04-2026/Esercizi/Cicli/Esercizio_4.py
+++
@@ -1,8 +0,0 @@
-# Chiediamo all'utente di scrivere una parola
-parola = input("Scrivi una parola: ")
-
-# Il ciclo itera su ogni lettera
-for lettera in parola:
-
-    # Stampa ogni lettera su una riga diversa
-    print(lettera)
