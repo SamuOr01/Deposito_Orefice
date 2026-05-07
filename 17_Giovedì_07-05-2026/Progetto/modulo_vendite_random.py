ab3692fe8590d2f25e38755e6979bdd1267148e2
Samuele Orefice <samu.or01@gmail.com>
2026-05-07 20:44:04 +0200

    Aggiornato modulo_vendite random

--- 17_Giovedì_07-05-2026/Progetto/modulo_vendite_random.py
+++ 17_Giovedì_07-05-2026/Progetto/modulo_vendite_random.py
@@ -69,78 +69,85 @@ class Vendite_Random:
         self._codice = 0
         self._lista_catalogo = []
 
-        def random_caratteristiche_capo_principale(self):
-            # Generazione casuale delle caratteristiche
-            self._codice += 1
-            tessuto = random.choice(self._lista_tessuti)
-            colore = random.choice(self._lista_colori)
-            taglia = random.choice(self._lista_taglie)
-            tipo_taglio = random.choice(self._lista_tipo_taglio)
-            numero_bottoni = random.randint(1, 3)
-            è_reversibile = random.choice([True, False])
+    def random_caratteristiche_capo_principale(self):
+        # Generazione casuale delle caratteristiche
+        self._codice += 1
+        tessuto = random.choice(self._lista_tessuti)
+        colore = random.choice(self._lista_colori)
+        taglia = random.choice(self._lista_taglie)
+        tipo_taglio = random.choice(self._lista_tipo_taglio)
+        numero_bottoni = random.randint(1, 3)
+        è_reversibile = random.choice([True, False])
 
 
-            return self._codice, tessuto, colore, taglia, tipo_taglio, numero_bottoni, è_reversibile
+        return self._codice, tessuto, colore, taglia, tipo_taglio, numero_bottoni, è_reversibile
 
-        def random_caratteristiche_finitura(codice, lista_tessuti, lista_colori, lista_larghezze, lista_tipo_chiusura):
+    def random_caratteristiche_finitura(self):
+        # Generazione casuale delle caratteristiche
+        self._codice += 1
+        tessuto = random.choice(self._lista_tessuti)
+        colore = random.choice(self._lista_colori)
+        larghezza = random.choice(self._lista_larghezze)
+        tipo_chiusura = random.choice(self._lista_tipo_chiusura)
+        ha_piega_decorativa = random.choice([True, False])
 
-            # Generazione casuale delle caratteristiche
-            codice += 1
-            tessuto = random.choice(lista_tessuti)
-            colore = random.choice(lista_colori)
-            larghezza = random.choice(lista_larghezze)
-            tipo_chiusura = random.choice(lista_tipo_chiusura)
-            ha_piega_decorativa = random.choice([True, False])
+        return self._codice, tessuto, colore, larghezza, tipo_chiusura, ha_piega_decorativa
 
-            return codice, tessuto, colore, larghezza, tipo_chiusura, ha_piega_decorativa
+    def creazione_casuale(self):
 
-        def creazione_casuale(self):
+        while True:
 
-            while True:
+            scelta = input("Cosa desidera acquistare?"
+                        "\n1. Capo Principale"
+                        "\n2. Finitura"
+                        "\n3. Esci"
+                        "\n\n> "
+                        )
 
-                scelta = input("Cosa desidera acquistare?"
-                            "\n1. Capo Principale"
-                            "\n2. Finitura"
-                            "\n\n> "
-                            )
+            match scelta:
 
-                match scelta:
-                    case "1":
-                        codice, tessuto, colore, taglia, tipo_taglio, numero_bottoni, è_reversibile = random_caratteristiche_capo_principale(codice, self._lista_tessuti, self._lista_colori, self._lista_taglie, self._lista_tipo_taglio)
-                        capo_principale_scelto = random.choice(self._lista_capo_principale)
+                case "1":
+                    codice, tessuto, colore, taglia, tipo_taglio, numero_bottoni, è_reversibile = self.random_caratteristiche_capo_principale()
+                    capo_principale_scelto = random.choice(self._lista_capo_principale)
 
-                        if capo_principale_scelto == "Giacca":
-                            capo_principale = Giacca(codice, "Giacca", tessuto, colore, taglia, self._prezzo_giacca, numero_bottoni)
-                            print(f"Hai scelto: {capo_principale._nome} - Costo totale: {capo_principale.costo()}€")
-                            self._lista_catalogo.append(capo_principale)
+                    if capo_principale_scelto == "Giacca":
+                        capo_principale = Giacca(codice, "Giacca", tessuto, colore, taglia, self._prezzo_giacca, numero_bottoni)
+                        print(f"Hai scelto: {capo_principale._nome} - Costo totale: {capo_principale.costo()}€")
+                        self._lista_catalogo.append(capo_principale)
 
-                        elif capo_principale_scelto == "Pantalone":
-                            capo_principale = Pantalone(codice, "Pantalone", tessuto, colore, taglia, self._prezzo_pantalone, tipo_taglio)
-                            print(f"Hai scelto: {capo_principale._nome} - Costo totale: {capo_principale.costo()}€")
-                            self._lista_catalogo.append(capo_principale)
+                    elif capo_principale_scelto == "Pantalone":
+                        capo_principale = Pantalone(codice, "Pantalone", tessuto, colore, taglia, self._prezzo_pantalone, tipo_taglio)
+                        print(f"Hai scelto: {capo_principale._nome} - Costo totale: {capo_principale.costo()}€")
+                        self._lista_catalogo.append(capo_principale)
 
-                        elif capo_principale_scelto == "Gilet":
-                            capo_principale = Gilet(codice, "Gilet", tessuto, colore, taglia, self._prezzo_gilet, è_reversibile)
-                            print(f"Hai scelto: {capo_principale._nome} - Costo totale: {capo_principale.costo()}€")
-                            self._lista_catalogo.append(capo_principale)
-                    case "2":
-                        codice, tessuto, colore, larghezza, tipo_chiusura, ha_piega_decorativa = random_caratteristiche_finitura(codice, self._lista_tessuti, self._lista_colori, self._lista_larghezze, self._lista_tipo_chiusura)
-                        finitura_scelta = random.choice(self._lista_finitura)
+                    elif capo_principale_scelto == "Gilet":
+                        capo_principale = Gilet(codice, "Gilet", tessuto, colore, taglia, self._prezzo_gilet, è_reversibile)
+                        print(f"Hai scelto: {capo_principale._nome} - Costo totale: {capo_principale.costo()}€")
+                        self._lista_catalogo.append(capo_principale)
 
-                        if finitura_scelta == "Cravatta":
-                            finitura = Cravatta(codice, "Cravatta", tessuto, colore, larghezza, self._prezzo_cravatta)
-                            print(f"Hai scelto: {finitura._nome} - Costo totale: {finitura.costo()}€")
-                            self._lista_catalogo.append(finitura)
+                case "2":
+                    codice, tessuto, colore, larghezza, tipo_chiusura, ha_piega_decorativa = self.random_caratteristiche_finitura()
+                    finitura_scelta = random.choice(self._lista_finitura)
 
-                        elif finitura_scelta == "Papillon":
-                            finitura = Papillon(codice, "Papillon", tessuto, colore, self._prezzo_papillon, tipo_chiusura)
-                            print(f"Hai scelto: {finitura._nome} - Costo totale: {finitura.costo()}€")
-                            self._lista_catalogo.append(finitura)
+                    if finitura_scelta == "Cravatta":
+                        finitura = Cravatta(codice, "Cravatta", tessuto, colore, self._prezzo_cravatta, larghezza)
+                        print(f"Hai scelto: {finitura._nome} - Costo totale: {finitura.costo()}€")
+                        self._lista_catalogo.append(finitura)
 
-                        elif finitura_scelta == "Pochette":
-                            finitura = Pochette(codice, "Pochette", tessuto, colore, self._prezzo_pochette, ha_piega_decorativa)
-                            print(f"Hai scelto: {finitura._nome} - Costo totale: {finitura.costo()}€")
-                            self._lista_catalogo.append(finitura)
-                    case _:
-                        print("Scelta non valida. Riprova.")
-                        continue
+                    elif finitura_scelta == "Papillon":
+                        finitura = Papillon(codice, "Papillon", tessuto, colore, self._prezzo_papillon, tipo_chiusura)
+                        print(f"Hai scelto: {finitura._nome} - Costo totale: {finitura.costo()}€")
+                        self._lista_catalogo.append(finitura)
+
+                    elif finitura_scelta == "Pochette":
+                        finitura = Pochette(codice, "Pochette", tessuto, colore, self._prezzo_pochette, ha_piega_decorativa)
+                        print(f"Hai scelto: {finitura._nome} - Costo totale: {finitura.costo()}€")
+                        self._lista_catalogo.append(finitura)
+
+                case "3":
+                    print("Grazie per aver acquistato!")
+                    break
+
+                case _:
+                    print("Scelta non valida. Riprova.")
+                    continue
