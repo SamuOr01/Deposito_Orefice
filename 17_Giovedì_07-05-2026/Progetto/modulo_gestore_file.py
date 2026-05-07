f4473b89b1a27f5ec0e75bd56ca2c8a343c3025f
Samuele Orefice <samu.or01@gmail.com>
2026-05-07 20:55:58 +0200

    modulo_gestore_file aggiornato

--- 17_Giovedì_07-05-2026/Progetto/modulo_gestore_file.py
+++ 17_Giovedì_07-05-2026/Progetto/modulo_gestore_file.py
@@ -3,16 +3,3 @@ def salva_catalogo(lista_catalogo):
     with open("catalogo.txt", "w", encoding="utf-8") as f:
         for item in lista_catalogo:
             f.write(f"{item}\n")
-
-def carica_catalogo():
-
-    lista_catalogo = []
-
-    try:
-        with open("catalogo.txt", "r", encoding="utf-8") as f:
-            for line in f:
-                lista_catalogo.append(line.strip())
-    except FileNotFoundError:
-        pass
-
-    return lista_catalogo
