bd7850963b260e67e0ac7214cfc9e76630956ee1
Samuele Orefice <samu.or01@gmail.com>
2026-05-08 11:47:17 +0200

    Creato GestoreFile e le due funzioni leggi_classe e crea_classe

---
+++ TestPratici/Test_08-05-2026/GestoreFile.py
@@ -0,0 +1,52 @@
+
+# FUNZIONI PER LA GESTIONE DELLE CLASSI
+
+# Funzione per scrivere i dati nel file
+def crea_classe(numero_studenti, nome):
+    classe = []
+    for i in range(numero_studenti):
+        studente = {
+            "id": i+1,
+            "nome": input(f"Inserisci il nome dello studente {i+1}: "),
+            "cognome": input(f"Inserisci il cognome dello studente {i+1}: "),
+            "età": input(f"Inserisci l'età dello studente {i+1}: "),
+            "classe": nome,
+            "media": input(f"Inserisci la media dello studente {i+1}: ")
+        }
+        classe.append(studente)
+    
+    with open(f"Classe_{nome}.txt", "w", encoding="utf-8") as file:
+        for studente in classe:
+            file.write(f"{studente['id']},{studente['nome']},{studente['cognome']},{studente['età']},{studente['classe']},{studente['media']}\n")
+    
+    return classe
+
+# Funzione per leggere i dati dal file
+def leggi_classe(nome):
+    classe = []
+    with open(f"Classe_{nome}.txt", "r", encoding="utf-8") as file:
+        righe = file.readlines()
+
+        for riga in righe:
+            dati = riga.strip().split(",")
+
+            studente = {
+                "id": dati[0],
+                "nome": dati[0],
+                "cognome": dati[1],
+                "età": dati[2],
+                "classe": dati[3],
+                "media": dati[4]
+            }
+
+            classe.append(studente)
+
+    return classe
+
+def mostra_classe():
+    pass
+
+def elimina_classe():
+    pass
+
+# FUNZIONI PER LA GESTIONE DEGLI STUDENTI
